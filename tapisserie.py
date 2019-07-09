7b18bd4fbf38e86af87a66cc59357a0ecd0914bc
taokann.one <site@taokann.one>
2019-07-09 13:30:11 +0200

    Initial commit - welcome message

---
+++ tapisserie.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  9 13:17:46 2019
+
+@author: taokann
+"""
+
+import pymsgbox as msg
+
+msg.alert("Welcome ! This is Tapisserie, the interactive wallpaper manager","Tapisserie - Step 1/1 - Welcome","Next")
